4b61c4fe2a543d75960f453519aa98222fb353ee
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2025-04-27 18:50:48 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,4 +28,23 @@ if ('serviceWorker' in navigator) {
     .then(() => console.log('Service Worker registrado'))
     .catch((error) => console.error('Error:', error));
 }
+let deferredPrompt;
 
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  // Muestra tu botón de instalación personalizado
+  const installButton = document.getElementById('install-button');
+  installButton.style.display = 'block';
+});
+
+// Al hacer clic en el botón de instalación
+document.getElementById('install-button').addEventListener('click', () => {
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('¡Usuario aceptó la instalación!');
+    }
+    deferredPrompt = null;
+  });
+});
